d0b00e1b1c3661887e941b5caf49571ae6157bc6
Your Name <xzhao00@stuy.edu>
2019-02-28 16:09:58 -0500

    version0

---
+++ Pet.java
@@ -0,0 +1,3 @@
+public class Pet extends Animal{
+//comment to test the futility of editing Holmes' repo
+}
---
+++ UserOfSpeakers.java
@@ -0,0 +1,6 @@
+public class UserOfSpeakers {
+    public static void main (String[] commandLine){
+	Animal ret = new Retriever();
+	System.out.println(Animal.speak());
+    }
+}
